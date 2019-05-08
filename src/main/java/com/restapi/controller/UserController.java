53e630f722c1536c8471d4ef6fa68dd36ac38c41
Alphind- Kathiravan <kathiravan1797@gmail.com>
2019-05-08 18:29:27 +0530

    cross origin hosting

--- src/main/java/com/restapi/controller/UserController.java
+++ src/main/java/com/restapi/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class UserController {
 	private UserService userService;
 	
 	@SuppressWarnings("unchecked")
+	@CrossOrigin(origins="http://localhost:4200") 
 	@RequestMapping(value = "/login" ,method = {RequestMethod.POST},  produces="application/json", consumes="application/json")
 	public ResponseEntity<List<Users>> loginUser(@RequestParam("uname") String uname, @RequestParam("pass") String pass) {
 		List<Users> user = userService.checkUser(uname, pass);
