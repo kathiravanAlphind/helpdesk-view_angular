b351fcd478ecf903ebdc02c2960815903eefede6
Alphind- Kathiravan <kathiravan1797@gmail.com>
2019-05-10 11:46:57 +0530

    cross origin controller

--- src/main/java/com/restapi/controller/UserController.java
+++ src/main/java/com/restapi/controller/UserController.java
@@ -5,8 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +21,14 @@ public class UserController {
 	private UserService userService;
 	
 	@SuppressWarnings("unchecked")
-	@CrossOrigin(origins="http://localhost:4200") 
-	@RequestMapping(value = "/login" ,method = {RequestMethod.POST},  produces="application/json", consumes="application/json")
+	@CrossOrigin("*")
+	@GetMapping(value = "/login")
 	public ResponseEntity<List<Users>> loginUser(@RequestParam("uname") String uname, @RequestParam("pass") String pass) {
 		List<Users> user = userService.checkUser(uname, pass);
 		if(!user.isEmpty())
 		{
 			return ResponseEntity.ok().body(user);
+					
 		}
 		
 		else
