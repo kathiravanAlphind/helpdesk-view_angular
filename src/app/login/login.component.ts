20124951f72b2dc45d9189de25dc5f61bf17379e
Alphind- Kathiravan <kathiravan1797@gmail.com>
2019-05-10 19:07:19 +0530

    final build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,21 +13,44 @@ import {ToastrService} from 'ngx-toastr';
 export class LoginComponent implements OnInit{
 
 
-    users : Login[];
+    users =  new Array<Login>();
     uname: string;
     pass: string;
 
     constructor(private _loginservice: LoginService, private toastr: ToastrService){}
 
     ngOnInit():void{
-        
+        let mobj = localStorage.getItem('name');
+        if(mobj == this.uname){
+            console.log("login success");
+        }
     }
     
     checkUser(){
-        this._loginservice.checkUser(this.uname, this.pass).subscribe(userData => {
-            this.users = userData, 
-            console.log(userData)})
+        if(this.uname!= null && this.pass!=null){
+            localStorage.setItem('name', this.uname);
+            this._loginservice.checkUser(this.uname, this.pass)
+        .subscribe((userData) => 
+        {this.users = userData,
+             console.log(userData)
+             let mobj = localStorage.getItem('name');
+             if(mobj == this.uname){
+                 console.log("login success");
+             }
+            
+            },
+             
+             (error) =>  
+            {console.log(error);
+            });
+        }
+        //this._loginservice.checkUser(this.uname, this.pass)
+        //.subscribe((userData) => {this.users = userData, console.log(userData)},(error) => {console.log(error);});
+            //this.toastr.error("Password missing","Field required");
+
+        else{
             this.toastr.error("Password missing","Field required");
+        }
     }
 
 }
